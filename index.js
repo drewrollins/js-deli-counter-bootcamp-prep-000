199b77076cc39349df2697b18fe7b660b1485919
Drew Rollins <drewrollins14@gmail.com>
2018-05-16 05:33:14 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,25 @@
+var katzDeli = [];
+
+function takeANumber(katzDeli, name) {
+  katzDeli.push(`${name}`);
+  return (`Welcome, ${name}. You are number ${katzDeli.length} in line.`);
+}
+
+
+function nowServing(katzDeliLine) {
+  if(katzDeliLine.length === 0) {
+    return "There is nobody waiting to be served!";
+  }
+  var name = katzDeliLine.shift();
+    return `Currently serving ${name}.`;
+  
+}
+
+function currentLine(katzDeliLine) {
+  if(katzDeliLine.length === 0) {
+    return "The line is currently empty.";
+  }
+  for(var i = 0; i < katzDeliLine.length; i++) {
+    
+  }
+}
