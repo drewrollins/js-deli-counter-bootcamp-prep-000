fed6c81f27650114967e1dfe6cea5d120e8bb52d
Drew Rollins <drewrollins14@gmail.com>
2018-05-16 06:26:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -24,7 +24,9 @@ function currentLine(katzDeliLine) {
     return "The line is currently empty.";
   }
   for(var i = 0; i < katzDeliLine.length; i++) {
-    
+    if (position === katzDeliLine.length) {
+      
+    }
   }
 }
 
